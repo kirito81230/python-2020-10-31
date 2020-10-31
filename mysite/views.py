6fc31dccfb775f5673f8d363f71e1fa093d9dea8
kirito81230 <andy35tsai@gmail.com>
2020-10-31 17:09:06 +0800

    first commit

---
+++ mysite/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+# Create your views here.
+def index(request):
+    lucky_no = random.randint(1,42)
+    fruit = random.choice(["蘋果","鳳梨","香蕉","芭樂","芒果","柚子","橘子","香瓜","西瓜","葡萄"])
+    numbers = list()
+    for i in range(6):
+        numbers.append(random.randint(1,42))
+    return render(request,"index.html",locals())
+    #return HttpResponse("<h3>您今天的幸運號碼是：{}<h3><h3>您今天的幸運水果是：{}<h3>".format(lucky_no,fruit))
+   
